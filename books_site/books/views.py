1889e8c55055a7db4f9486659c72954f176e2e65
chelseacastelli <chelsea.castelli@students.makeschool.com>
2020-04-24 14:52:06 -0400

    [UPDATE] Create book list view

--- books_site/books/views.py
+++ books_site/books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+
+def home(request):
+    context = {
+        'books': Book.objects.all()
+    }
+    return render(request, 'home.html', context)
